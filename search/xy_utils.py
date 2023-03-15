959a5e3e0a06a973fd87ed812bdc1819c89f2451
Xin Yu <chuaxc@unimelb.edu.au>
2023-03-15 14:18:28 +1100

    Implemented is_goal_reached() function

--- search/xy_utils.py
+++ search/xy_utils.py
@@ -10,4 +10,9 @@ def is_goal_reached(state: BoardState) -> bool:
     """
     Checks if the goal state (all pieces on board are Red) has been reached. Returns `True` if goal has been reached, `False` otherwise.
     """
-    print("is goal reached")
+    dim = 7
+    for r in range(0, dim):
+        for q in range(0, dim):
+            if (r, q) in state and state[(r, q)][0] == "b":
+                return False
+    return True
