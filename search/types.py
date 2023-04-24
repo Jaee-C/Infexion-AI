54e9d30b16b99a221c74ee1abef4f747e0647b8d
Daniel Chin <wengjae@gmail.com>
2023-04-24 15:42:26 +1000

    part B initial commit

--- search/types.py
+++
@@ -1,9 +0,0 @@
-# BoardState:
-# - key: coordinate - (r, q)
-# - value: cell state - (color, power)
-BoardState = dict[tuple, tuple]
-
-# Action:
-# (r, q, dr, dq), where dr is the r direction in which the action is to be taken
-# and dq is the q direction in which the action is to be taken
-Action = (int, int, int, int)
